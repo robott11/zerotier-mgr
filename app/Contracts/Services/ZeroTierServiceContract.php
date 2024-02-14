9e808ef265545bec99fff76b1c894be28924a0b0
Robott11 <contato.robott@gmail.com>
2024-02-14 00:09:46 -0300

    feat: zeroTierServiceContract updated

--- app/Contracts/Services/ZeroTierServiceContract.php
+++ app/Contracts/Services/ZeroTierServiceContract.php
@@ -7,4 +7,8 @@ use Minicli\ServiceInterface;
 interface ZeroTierServiceContract extends ServiceInterface
 {
     public function getNetworks(): array;
+
+    public function getMembers(string $network): array;
+    
+    public function authorizeMember(string $network, string $member): bool;
 }
